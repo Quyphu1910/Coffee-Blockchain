d645a00125ae83b37a3cd011a0850eb75f42de20
QuyPhu <toquyphu5@.com>
2025-03-03 22:10:40 +0700

    feat(fix buy product, add product)

--- backend/controllers/coffeeShopController.js
+++ backend/controllers/coffeeShopController.js
@@ -1,6 +1,6 @@
-const CoffeeShop = require('../models/coffeeShopModel');
-const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
+const CoffeeShop = require("../models/coffeeShopModel");
+const Web3 = require("web3");
+const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
 
 exports.registerUser = async (req, res) => {
   const { address } = req.body;
@@ -8,7 +8,14 @@ exports.registerUser = async (req, res) => {
   const fromAddress = accounts.includes(address) ? address : accounts[0];
   const instance = await CoffeeShop.deployed();
   await instance.registerUser({ from: fromAddress });
-  res.send('User registered');
+  res.send("User registered");
+};
+
+exports.getProduct = async (req, res) => {
+  const { productId } = req.params;
+  const instance = await CoffeeShop.deployed();
+  const product = await instance.products(productId);
+  res.json(product);
 };
 
 exports.addProduct = async (req, res) => {
@@ -17,7 +24,7 @@ exports.addProduct = async (req, res) => {
   const fromAddress = accounts.includes(address) ? address : accounts[0];
   const instance = await CoffeeShop.deployed();
   await instance.addProduct(name, price, { from: fromAddress });
-  res.send('Product added');
+  res.send("Product added");
 };
 
 exports.updateProduct = async (req, res) => {
@@ -26,7 +33,7 @@ exports.updateProduct = async (req, res) => {
   const fromAddress = accounts.includes(address) ? address : accounts[0];
   const instance = await CoffeeShop.deployed();
   await instance.updateProduct(productId, name, price, { from: fromAddress });
-  res.send('Product updated');
+  res.send("Product updated");
 };
 
 exports.deleteProduct = async (req, res) => {
@@ -35,7 +42,7 @@ exports.deleteProduct = async (req, res) => {
   const fromAddress = accounts.includes(address) ? address : accounts[0];
   const instance = await CoffeeShop.deployed();
   await instance.deleteProduct(productId, { from: fromAddress });
-  res.send('Product deleted');
+  res.send("Product deleted");
 };
 
 exports.buyProduct = async (req, res) => {
@@ -43,8 +50,31 @@ exports.buyProduct = async (req, res) => {
   const accounts = await web3.eth.getAccounts();
   const fromAddress = accounts.includes(address) ? address : accounts[0];
   const instance = await CoffeeShop.deployed();
-  await instance.buyProduct(productId, { from: fromAddress, value: web3.utils.toWei(value, 'ether') });
-  res.send('Product purchased');
+
+  console.log("productId from request:", productId);
+  console.log("address from request:", address);
+  console.log("value from request:", value);
+  console.log("fromAddress being used:", fromAddress);
+
+  // Fetch the product price from the contract
+  const product = await instance.products(productId);
+  const productPrice = product.price;
+  console.log("productPrice from contract:", productPrice.toString());
+
+  // Convert the value to Wei and compare with product price
+  const valueInWei = web3.utils.toWei(value, "ether");
+  console.log("valueInWei calculated:", valueInWei);
+
+  if (valueInWei !== productPrice.toString()) {
+    console.log("Giá trị không khớp! Lỗi 400 trả về."); // Thêm log này
+    return res.status(400).send("Incorrect value sent");
+  }
+
+  await instance.buyProduct(productId, {
+    from: fromAddress,
+    value: valueInWei,
+  });
+  res.send("Product purchased");
 };
 
 exports.login = async (req, res) => {
@@ -54,9 +84,9 @@ exports.login = async (req, res) => {
   const instance = await CoffeeShop.deployed();
   const isRegistered = await instance.isUserRegistered(fromAddress);
   if (isRegistered) {
-    res.send('Login successful');
+    res.send("Login successful");
   } else {
-    res.status(401).send('User not registered');
+    res.status(401).send("User not registered");
   }
 };
 
--- backend/routes/coffeeShopRoutes.js
+++ backend/routes/coffeeShopRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const coffeeShopController = require('../controllers/coffeeShopController');
 
 router.post('/register', coffeeShopController.registerUser);
+router.get('/product/:productId', coffeeShopController.getProduct);
 router.post('/addProduct', coffeeShopController.addProduct);
 router.post('/updateProduct', coffeeShopController.updateProduct);
 router.post('/deleteProduct', coffeeShopController.deleteProduct);
